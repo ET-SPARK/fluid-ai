d23a35555de85087f4fbc1e6ad46f2c434d611c6
ET-SPARK <samuelwoyesso2016@gmail.com>
2024-10-02 20:18:12 +0300

    fixed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,6 @@
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HeroSecond from "@/components/HeroSecond";
-import Image from "next/image";
 import React from "react";
-import HeroBg from "../../assets/heroBg.png";
-import HeroTh from "@/components/HeroTh";
 import Faq from "@/components/Faq";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -19,7 +15,6 @@ function HomePage() {
       <Faq />
       <Contact />
       <Footer />
-      {/* <HeroTh /> */}
     </div>
   );
 }
